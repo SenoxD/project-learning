35df7be4f494ab1622c04968b0e95920a08a2e38
Roxana Sapun <sapun_roxana@yahoo.com>
2023-07-13 13:18:52 +0300

    I ve created the about page elements

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
+  constructor(private renderer: Renderer2) {}
 
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    console.log('Scroll event triggered');
+    const scrollHeight = document.body.scrollHeight;
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    // Calculate the middle position of the page
+    const middlePosition = scrollHeight / 2;
+
+    // If scroll position is beyond the middle, scroll to the bottom
+    if (scrollTop > middlePosition) {
+      window.scrollTo({ top: scrollHeight, behavior: 'smooth' });
+    }
+  }
 }
